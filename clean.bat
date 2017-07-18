afaedd09932bb0d132091d8cbc43740f1037a930
sergeeo <sergeeo@gmail.com>
2017-07-18 18:30:51 +0200

    "compiled parser" clean.bat compile.bat working with my version of gbdk sample tune notepad++ mml definition by TacticalBread

---
+++ clean.bat
@@ -0,0 +1,5 @@
+@echo off
+del driver\*.o
+del driver\*.asm
+del rom.gb
+echo Everything's clean now
--- compile.bat
+++ compile.bat
@@ -7,12 +7,12 @@ if %errorlevel% neq 0 (
 	exit /b %errorlevel%
 )
 
-lcc.exe -c driver\player.asm -o driver\player.o
-lcc.exe -c driver\music.asm -o driver\music.o
+lcc.exe -c driver\player.c -o driver\player.o
+lcc.exe -c driver\music.c -o driver\music.o
 lcc.exe -c driver\song.asm -o driver\song.o
-lcc.exe -c driver\freq.asm -o driver\freq.o
-lcc.exe -c driver\noisefreq.asm -o driver\noisefreq.o
-lcc.exe -c driver\vib.asm -o driver\vib.o
+lcc.exe -c driver\freq.c -o driver\freq.o
+lcc.exe -c driver\noisefreq.c -o driver\noisefreq.o
+lcc.exe -c driver\vib.c -o driver\vib.o
 lcc.exe driver\player.o driver\music.o driver\song.o driver\freq.o driver\noisefreq.o driver\vib.o -o rom.gb
 
 if %errorlevel% neq 0 (
@@ -22,4 +22,5 @@ if %errorlevel% neq 0 (
 )
 
 echo %~nx0 compiled to rom.gb successfully!
-pause
+
+C:\gbdk\bin\bgb\BGB.exe rom.gb
